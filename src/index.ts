034becd2c398bcf39feade826ae896f2abc0620c
Yury Kozyrev <y.kozyrev@travelaudience.com>
2019-01-22 14:28:35 +0100

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import * as TesseractTypes from "tesseract.js";
 declare var window: any;
-const _instance = window ? require("tesseract.js/dist/tesseract") : require('tesseract.js'); 
+const _instance = typeof window !== 'undefined' ? require("tesseract.js/dist/tesseract") : require('tesseract.js'); 
 export const Tesseract: TesseractTypes.TesseractStatic =  _instance;
