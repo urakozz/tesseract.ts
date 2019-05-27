173d881ff3ec5ac498f04b03ed6f2df8f1f67d53
Yury Kozyrev <y.kozyrev@travelaudience.com>
2019-05-27 15:45:42 +0200

    npm up

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var _instance = window ? require("tesseract.js/dist/tesseract") : require('tesseract.js');
+var _instance = typeof window !== 'undefined' ? require("tesseract.js/dist/tesseract") : require('tesseract.js');
 exports.Tesseract = _instance;
 //# sourceMappingURL=index.js.map
